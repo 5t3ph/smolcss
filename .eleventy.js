db22a9b2929b96b9d2e21ef3263e76619e4f8377
5t3ph <thinkdobecreate@gmail.com>
2021-02-11 23:17:35 -0600

    Add RSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
   eleventyConfig.addPassthroughCopy("./src/*.png");
 
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(pluginRss);
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,3 +1,6 @@
 module.exports = {
   url: process.env.URL || "http://localhost:8080",
+  siteName: "SmolCSS",
+  siteDescription: "Minimal snippets for modern CSS layouts and components",
+  authorName: "Stephanie Eckles",
 };
